692f238c070e72bb8969c0f2328f75e56366631f
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2024-08-13 07:47:11 +0200

    ✨ feat: implement filter and filter operator

---
+++ criteria_pattern/filter.py
@@ -0,0 +1,72 @@
+"""
+This module contains the Filter class.
+"""
+
+from typing import Generic, TypeVar, override
+
+from .filter_operator import FilterOperator
+
+T = TypeVar('T')
+
+
+class Filter(Generic[T]):
+    """
+    Filter class.
+    """
+
+    __field: str
+    __operator: FilterOperator
+    __value: T
+
+    def __init__(self, field: str, operator: FilterOperator, value: T) -> None:
+        """
+        Filter constructor.
+
+        Args:
+            field (str): Field name.
+            operator (FilterOperator): Filter operator.
+            value (T): Filter value.
+        """
+        self.__field = field
+        self.__operator = operator
+        self.__value = value
+
+    @override
+    def __repr__(self) -> str:
+        """
+        Get string representation of Filter.
+
+        Returns:
+            str: String representation of Filter.
+        """
+        return f'{self.field!r} {self.operator} {self.value!r}'
+
+    @property
+    def field(self) -> str:
+        """
+        Get field.
+
+        Returns:
+            str: Field name.
+        """
+        return self.__field
+
+    @property
+    def operator(self) -> FilterOperator:
+        """
+        Get operator.
+
+        Returns:
+            FilterOperator: Filter operator.
+        """
+        return self.__operator
+
+    @property
+    def value(self) -> T:
+        """
+        Get value.
+
+        Returns:
+            T: Filter value.
+        """
+        return self.__value
