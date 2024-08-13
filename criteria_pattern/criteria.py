341a072af6f7f1d155cf1b269293bb82a389949f
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2024-08-13 20:10:59 +0200

    ✨ feat: implement and operator to criteria class

--- criteria_pattern/criteria.py
+++ criteria_pattern/criteria.py
@@ -38,6 +38,50 @@ class Criteria(Generic[T]):
         """
         return f'<Criteria(filters={self.__filters})>'
 
+    def __and__(self, criteria: Criteria[T]) -> Criteria[T]:
+        """
+        Combine two criteria with AND operator. It merges the filters from both criteria into a single Criteria object.
+
+        Args:
+            criteria (Criteria): Another criteria.
+
+        Returns:
+            Criteria: Combined criteria.
+
+        Example:
+        ```python
+        criteria1 = Criteria(filters=[filter1])
+        criteria2 = Criteria(filters=[filter2])
+
+        # both are equivalent
+        criteria3 = criteria1 & criteria2
+        criteria3 = Criteria(filters=[filter1, filter2])
+        ```
+        """
+        return Criteria(filters=self.__filters + criteria.filters)
+
+    def add_(self, criteria: Criteria[T]) -> Criteria[T]:
+        """
+        Combine two criteria with AND operator.
+
+        Args:
+            criteria (Criteria): Another criteria.
+
+        Returns:
+            Criteria: Combined criteria.
+
+        Example:
+        ```python
+        criteria1 = Criteria(filters=[filter1])
+        criteria2 = Criteria(filters=[filter2])
+
+        # both are equivalent
+        criteria3 = criteria1.add_(criteria2)
+        criteria3 = Criteria(filters=[filter1, filter2])
+        ```
+        """
+        return self & criteria
+
     @property
     def filters(self) -> list[Filter[T]]:
         """
