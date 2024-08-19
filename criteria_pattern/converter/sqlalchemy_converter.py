a153893b8400c2b97c057a71a6407c4e70e5361c
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2024-08-19 16:21:35 +0200

    ✨ feat: add column_mapping parameter so frontend names can be mapped in the backend models or tables

--- criteria_pattern/converter/sqlalchemy_converter.py
+++ criteria_pattern/converter/sqlalchemy_converter.py
@@ -25,35 +25,46 @@ class SQLAlchemyConverter:
     SQLAlchemy converter.
     """
 
-    def convert(self, criteria: Criteria, model: type[T]) -> Query[T]:
+    def convert(self, criteria: Criteria, model: type[T], column_mapping: dict[str, str] | None = None) -> Query[T]:
         """
         Convert the Criteria object to a SQLAlchemy Query.
 
         Args:
             criteria (Criteria): Criteria to convert.
             model (DeclarativeMeta): SQLAlchemy model.
+            alias_mapping (dict[str, str], optional): Alias for column names. Default to {}.
 
         Returns:
             Query: Query object.
         """
         query: Query[T] = Query(model)
-        filters = self._process_filters(criteria=criteria, model=model)
+
+        if column_mapping is None:
+            column_mapping = {}
+
+        filters = self._process_filters(criteria=criteria, model=model, column_mapping=column_mapping)
         if filters:
             query = query.filter(*filters)
 
-        orders = self._process_orders(criteria=criteria, model=model)
+        orders = self._process_orders(criteria=criteria, model=model, column_mapping=column_mapping)
         if orders:
             query = query.order_by(*orders)
 
         return query
 
-    def _process_filters(self, criteria: Criteria, model: type[T]) -> list[ColumnElement[bool]]:  # noqa: C901
+    def _process_filters(  # noqa: C901
+        self,
+        criteria: Criteria,
+        model: type[T],
+        column_mapping: dict[str, str],
+    ) -> list[ColumnElement[bool]]:
         """
         Process the Criteria and return a list of conditions.
 
         Args:
             criteria (Criteria): Criteria to process.
             model (DeclarativeMeta): SQLAlchemy model.
+            column_mapping (dict[str, str]): Alias for column names.
 
         Returns:
             list[BinaryExpression]: List of conditions.
@@ -61,21 +72,38 @@ class SQLAlchemyConverter:
         conditions: list[ColumnElement[bool]] = []
 
         if isinstance(criteria, AndCriteria):
-            left_conditions = self._process_filters(criteria=criteria.left, model=model)
-            right_conditions = self._process_filters(criteria=criteria.right, model=model)
+            left_conditions = self._process_filters(
+                criteria=criteria.left,
+                model=model,
+                column_mapping=column_mapping,
+            )
+            right_conditions = self._process_filters(
+                criteria=criteria.right,
+                model=model,
+                column_mapping=column_mapping,
+            )
             conditions.append(and_(*left_conditions, *right_conditions))
 
             return conditions
 
         if isinstance(criteria, OrCriteria):
-            left_conditions = self._process_filters(criteria=criteria.left, model=model)
-            right_conditions = self._process_filters(criteria=criteria.right, model=model)
+            left_conditions = self._process_filters(
+                criteria=criteria.left,
+                model=model,
+                column_mapping=column_mapping,
+            )
+            right_conditions = self._process_filters(
+                criteria=criteria.right,
+                model=model,
+                column_mapping=column_mapping,
+            )
             conditions.append(or_(*left_conditions, *right_conditions))
 
             return conditions
 
         for filter in criteria.filters:
-            field: Column[Any] = getattr(model, filter.field)
+            field_name = column_mapping.get(filter.field, filter.field)
+            field: Column[Any] = getattr(model, field_name)
             match filter.operator:
                 case FilterOperator.EQUAL:
                     conditions.append(field == filter.value)
@@ -133,13 +161,19 @@ class SQLAlchemyConverter:
 
         return conditions
 
-    def _process_orders(self, criteria: Criteria, model: type[T]) -> list[UnaryExpression[Any]]:
+    def _process_orders(
+        self,
+        criteria: Criteria,
+        model: type[T],
+        column_mapping: dict[str, str],
+    ) -> list[UnaryExpression[Any]]:
         """
         Process the Criteria and return a list of order fields.
 
         Args:
             criteria (Criteria): Criteria to process.
             model (DeclarativeMeta): SQLAlchemy model.
+            column_mapping (dict[str, str]): Alias for column names.
 
         Returns:
             list[Column]: List of order fields.
@@ -147,7 +181,8 @@ class SQLAlchemyConverter:
         orders = []
 
         for order in criteria.orders:
-            field: Column[Any] = getattr(model, order.field)
+            field_name = column_mapping.get(order.field, order.field)
+            field: Column[Any] = getattr(model, field_name)
             match order.direction:
                 case OrderDirection.ASC:
                     orders.append(field.asc())
