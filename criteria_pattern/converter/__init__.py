d6ad19e06af3b5361c3f7b8c08172928138f67ee
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2024-08-26 16:22:13 +0200

    ✨ feat: implement criteria to raw sql queries

--- criteria_pattern/converter/__init__.py
+++ criteria_pattern/converter/__init__.py
@@ -1,3 +1,7 @@
+from .sql_converter import SqlConverter
 from .sqlalchemy_converter import SQLAlchemyConverter
 
-__all__ = ('SQLAlchemyConverter',)
+__all__ = (
+    'SQLAlchemyConverter',
+    'SqlConverter',
+)
