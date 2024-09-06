31b6a0f86d9034b2d85df922d128c3b2d9fdc911
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2024-09-06 11:15:12 +0200

    ✨ refactor: change filter operator to their operators in sql

--- criteria_pattern/filter_operator.py
+++ criteria_pattern/filter_operator.py
@@ -2,29 +2,28 @@
 Filter operator module.
 """
 
-from enum import StrEnum, unique
+from enum import StrEnum
 
 
-@unique
 class FilterOperator(StrEnum):
     """
     FilterOperator enum class.
     """
 
-    EQUAL = 'eq'
-    NOT_EQUAL = 'neq'
-    GREATER = 'gt'
-    GREATER_OR_EQUAL = 'gte'
-    LESS = 'lt'
-    LESS_OR_EQUAL = 'lte'
-    LIKE = 'like'
-    IN = 'in'
-    NOT_IN = 'nin'
-    IS_NULL = 'is_null'
-    IS_NOT_NULL = 'is_not_null'
-    BETWEEN = 'between'
-    NOT_BETWEEN = 'not_between'
-    NOT_CONTAINS = 'not_contains'
-    STARTS_WITH = 'starts_with'
-    ENDS_WITH = 'ends_with'
-    CONTAINS = 'contains'
+    EQUAL = '='
+    NOT_EQUAL = '!='
+    GREATER = '>'
+    GREATER_OR_EQUAL = '>='
+    LESS = '<'
+    LESS_OR_EQUAL = '<='
+    LIKE = 'LIKE'
+    IN = 'IN'
+    NOT_IN = 'NOT IN'
+    IS_NULL = 'IS NULL'
+    IS_NOT_NULL = 'IS NOT NULL'
+    BETWEEN = 'BETWEEN'
+    NOT_BETWEEN = 'NOT BETWEEN'
+    CONTAINS = 'LIKE'
+    NOT_CONTAINS = 'NOT LIKE'
+    STARTS_WITH = 'LIKE'
+    ENDS_WITH = 'LIKE'
